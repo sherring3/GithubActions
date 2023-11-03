b7794d6af6009e9fba4afbfcbf92a8f3e119870c
sherring3 <102832288+sherring3@users.noreply.github.com>
2023-11-03 15:01:03 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -32,9 +32,9 @@ namespace GithubActionsLab
        [Test]
         public void Sub_Valid()
         {
-            Assert.AreEqual(3, Program.Subtract("1", "2"));
-            Assert.AreEqual(5, Program.Subtract("3", "2"));
-            Assert.AreEqual(12, Program.Subtract("5", "7"));
+            Assert.AreEqual(1, Program.Subtract("2", "1"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
         }
 
         [Test]
@@ -55,9 +55,9 @@ namespace GithubActionsLab
          [Test]
         public void Divide_Valid()
         {
-            Assert.AreEqual(3, Program.Divide("1", "2"));
-            Assert.AreEqual(5, Program.Divide("3", "2"));
-            Assert.AreEqual(12, Program.Divide("5", "7"));
+            Assert.AreEqual(0.5, Program.Divide("1", "2"));
+            Assert.AreEqual(1, Program.Divide("3", "3"));
+            Assert.AreEqual(2, Program.Divide("4", "2"));
         }
 
         [Test]
@@ -78,9 +78,9 @@ namespace GithubActionsLab
          [Test]
         public void Multiply_Valid()
         {
-            Assert.AreEqual(3, Program.Multiply("1", "2"));
-            Assert.AreEqual(5, Program.Multiply("3", "2"));
-            Assert.AreEqual(12, Program.Multiply("5", "7"));
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
         }
 
         [Test]
@@ -101,9 +101,9 @@ namespace GithubActionsLab
          [Test]
         public void Power_Valid()
         {
-            Assert.AreEqual(3, Program.Power("1", "2"));
-            Assert.AreEqual(5, Program.Power("3", "2"));
-            Assert.AreEqual(12, Program.Power("5", "7"));
+            Assert.AreEqual(1, Program.Power("1", "2"));
+            Assert.AreEqual(4, Program.Power("2", "2"));
+            Assert.AreEqual(8, Program.Power("2", "3"));
         }
 
         [Test]
